4a27f93d4a2e07f82b4d85d60d5ce997dc77d669
Bipin Maharjan <bopon.maharjan@gmail.com>
2023-09-05 12:50:01 +0545

    syncing files

--- backend/src/utils/helper/formatZodError.ts
+++
@@ -1,9 +0,0 @@
-import { ZodError } from "zod";
-import { fromZodError } from "zod-validation-error";
-
-export function formatZodError(error: ZodError) {
-  const validationError = fromZodError(error);
-  console.error(validationError);
-  const message = validationError.message.split(":").slice(1).join(":").trim();
-  return message.split(";");
-}
--- backend/src/utils/helper/validation.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export function validate(schema: z.Schema) {
-  return (req: any, res: any, next: any) => {
-    schema.parse(req.body);
-    next();
-  };
-}
