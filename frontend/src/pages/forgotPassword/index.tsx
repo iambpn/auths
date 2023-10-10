ade5ac6f0120034f68a26366422a2f83034f9d15
Bipin Maharjan <Bopon.maharjan@gmail.com>
2023-10-10 23:14:57 +0545

    completing forget password page

--- frontend/src/pages/forgotPassword/index.tsx
+++ frontend/src/pages/forgotPassword/index.tsx
@@ -1,11 +1,11 @@
-import SecurityQuestionForm from "@/components/securityQuestion/securityQuestion.form";
+import SecurityQuestionForm, { SecurityQuestionType } from "@/components/securityQuestion/securityQuestion.form";
 import SettingHeader from "@/components/settings/settingHeader";
+import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { MdArrowBack } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { z } from "zod";
@@ -32,6 +32,14 @@ export default function ForgotPassword() {
   });
   const navigate = useNavigate();
   const [step, setStep] = useState(1);
+  const [email, setEmail] = useState("");
+
+  const handleEmailSubmission: SubmitHandler<ValidationEmailType> = (values) => {
+    setEmail(values.email);
+    setStep((prev) => prev + 1);
+  };
+
+  const handleSecurityQuestionSubmission: SubmitHandler<SecurityQuestionType> = (values) => {};
 
   return (
     <div className='container h-full flex flex-col'>
@@ -41,23 +49,18 @@ export default function ForgotPassword() {
             <MdArrowBack className='w-5 h-5' />
           </span>
         </div>
-        <div className='pl-2'>
-          <SettingHeader description='Fill the form below to reset your password.' header='Forget Password' />
-        </div>
       </div>
-      <div className='flex justify-center items-center h-full'>
+      <div className='flex justify-center h-full pt-28'>
         {step === 1 && (
-          <div className='w-2/5'>
-            <SettingHeader description='Fill the form below to reset your password.' header='Forget Password' />
-
+          <div className='w-2/5 space-x-2'>
+            <SettingHeader description='Enter you email address to reset password' header='Forget Password' />
             <Form {...form}>
-              <form onSubmit={form.handleSubmit(() => {})}>
+              <form onSubmit={form.handleSubmit(handleEmailSubmission)} className='pt-2'>
                 <FormField
                   control={form.control}
                   name='email'
                   render={({ field }) => (
                     <FormItem>
-                      <Label>Email</Label>
                       <FormControl>
                         <Input type='email' className='w-full' placeholder='Email' {...field} />
                       </FormControl>
@@ -65,15 +68,17 @@ export default function ForgotPassword() {
                     </FormItem>
                   )}
                 />
+                <div className='pt-3 flex justify-end'>
+                  <Button type='submit'>Forgot Password</Button>
+                </div>
               </form>
             </Form>
           </div>
         )}
         {step === 2 && (
-          <div className='w-2/5'>
-            <h1 className='text-xl font-medium'>Security Question</h1>
-            <p className='text-muted-foreground'>Enter your security question answers</p>
-            <SecurityQuestionForm onSubmit={() => {}} questions={[]} defaultValues={{ question1: "", question2: "" }} />
+          <div className='w-2/5 space-x-2'>
+            <SettingHeader description='Enter your security question answers' header='Forget Password' />
+            <SecurityQuestionForm onSubmit={handleSecurityQuestionSubmission} questions={[]} defaultValues={{ question1: "", question2: "" }} disabled={true} />
           </div>
         )}
       </div>
