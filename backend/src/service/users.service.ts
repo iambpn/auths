f7b454efb1791b67b11b44340cf2877c787a1a60
Bipin Maharjan <bopon.maharjan@gmail.com>
2023-10-31 17:58:48 +0545

    implementing users api

--- backend/src/service/users.service.ts
+++ backend/src/service/users.service.ts
@@ -1,5 +1,6 @@
+import { eq } from "drizzle-orm";
 import { db } from "../schema/drizzle-migrate";
-import { UserSchema } from "../schema/drizzle-schema";
+import { RolesSchema, UserSchema } from "../schema/drizzle-schema";
 import { PaginationQuery } from "../utils/helper/parsePagination";
 
 export async function getAllUsers(paginationQuery: ReturnType<typeof PaginationQuery>) {
@@ -16,5 +17,10 @@ export async function getAllUsers(paginationQuery: ReturnType<typeof PaginationQ
     .limit(paginationQuery.limit)
     .offset(paginationQuery.skip);
 
-  return users;
+  return await Promise.all(
+    users.map(async (user) => {
+      const [role] = await db.select().from(RolesSchema).where(eq(RolesSchema.uuid, user.role)).limit(1);
+      return { ...user, role: role };
+    })
+  );
 }
--- frontend/src/pages/afterLogin/dashboard/users/index.tsx
+++ frontend/src/pages/afterLogin/dashboard/users/index.tsx
@@ -1,7 +1,9 @@
 import { AvatarComponent } from "@/components/avatar/avatar.component";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { axiosInstance } from "@/lib/axiosInstance";
 import { NavName } from "@/lib/navName";
 import { useAppStore } from "@/store/useAppStore";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
 export function Users() {
@@ -10,6 +12,29 @@ export function Users() {
   useEffect(() => {
     updateActiveNavLink(NavName.users);
   }, []);
+
+  const UsersQuery = useQuery<
+    {
+      role: {
+        uuid: string;
+        name: string;
+        createdAt: Date;
+        updatedAt: Date;
+        slug: string;
+      };
+      uuid: string;
+      email: string;
+      others: string | null;
+      createdAt: Date;
+      updatedAt: Date;
+    }[]
+  >({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/users?page=0&limit=10");
+      return res.data;
+    },
+  });
 
   return (
     <div className=''>
@@ -28,15 +53,18 @@ export function Users() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow key={""}>
-              <TableCell className='pl-2 md:pl-5'>
-                <AvatarComponent fallBackText='' link='' />
-              </TableCell>
-              <TableCell className='font-medium capitalize'>email@email.com</TableCell>
-              <TableCell>SuperAdmin</TableCell>
-              <TableCell>{new Date().toISOString()}</TableCell>
-              <TableCell>{new Date().toISOString()}</TableCell>
-            </TableRow>
+            {UsersQuery.data &&
+              UsersQuery.data.map((user) => (
+                <TableRow key={user.uuid}>
+                  <TableCell className='pl-2 md:pl-5'>
+                    <AvatarComponent fallBackText={user.email.split("@")[0].substring(0, 2)} link='' />
+                  </TableCell>
+                  <TableCell className='font-medium capitalize'>{user.email}</TableCell>
+                  <TableCell className='capitalize'>{user.role.name}</TableCell>
+                  <TableCell>{new Date(user.updatedAt).toISOString()}</TableCell>
+                  <TableCell>{new Date(user.createdAt).toISOString()}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </div>
